91b18decef8be24071d1daa1b3f4d9dcfc43d574
savespace <savespace@outlook.com>
2017-08-06 17:41:32 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
+var allNames = ['Asia','Kasia','Ola','Jola','Piotrek','Marek','Arek','Jarek'];
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames);
-console.log(allNames);
-
+console.log(femaleNames,maleNames);
+var arr = allNames.concat(femaleNames,maleNames);
+console.log(arr); // ['Asia','Kasia','Ola','Jola','Piotrek','Marek','Arek','Jarek']
 var newName = 'Marian';
+	
+	if (allNames.indexOf(newName) === -1) {
+		console.log('Baza zaktualizowana');
+	}
 
-if (allNames.indexOf(newName) === -1){
-var moreNames = allNames.push(newName);
-console.log('Zaktualizowano baze');
-}
-else {
-console.log('Baza pusta');
-}
+var allNames = ['Asia','Kasia','Ola','Jola','Piotrek','Marek','Arek','Jarek'];
+var newName = allNames.push('Marian');
+console.log(allNames);
